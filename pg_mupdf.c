2d9186e6baddab6d52661da31bde3148062bd660
RekGRpth <rekgrpth@gmail.com>
2019-07-15 22:01:02 +0500

    up

--- pg_mupdf.c
+++ pg_mupdf.c
@@ -96,7 +96,7 @@ EXTENSION(pg_mupdf) {
 //    output = fz_new_output_with_buffer(context, output_buffer);
     //fz_try(context) 
 //    document_writer = fz_new_document_writer(context, (const char *)output, output_type, options);// fz_catch(context) ereport(ERROR, (errmsg("fz_new_document_writer: %s", fz_caught_message(context))));
-    document_writer = fz_new_document_writer(context, "buf:", output_type, options);// fz_catch(context) ereport(ERROR, (errmsg("fz_new_document_writer: %s", fz_caught_message(context))));
+    document_writer = fz_new_document_writer(context, NULL, output_type, options);// fz_catch(context) ereport(ERROR, (errmsg("fz_new_document_writer: %s", fz_caught_message(context))));
     (void)runrange(document, range, document_writer);
 //    output_len = fz_buffer_extract(context, output_buffer, &output_data);
     output_len = fz_buffer_extract(context, input_buffer, &output_data);
