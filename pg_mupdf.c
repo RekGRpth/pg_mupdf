bbd41cfb97a6b7e4081c78f54761fb8cfe02dca3
RekGRpth <rekgrpth@gmail.com>
2019-07-17 08:16:17 +0500

    up

--- pg_mupdf.c
+++ pg_mupdf.c
@@ -26,7 +26,7 @@ void _PG_fini(void); void _PG_fini(void) {
 
 static void runpage(fz_document *doc, int number, fz_document_writer *wri) {
     fz_page *page = fz_load_page(ctx, doc, number - 1);
-    elog(LOG, "runpage: number=%i", number);
+//    elog(LOG, "runpage: number=%i", number);
     fz_try(ctx) {
         fz_rect mediabox = fz_bound_page(ctx, page);
         fz_device *dev = fz_begin_page(ctx, wri, mediabox);
@@ -41,7 +41,7 @@ static void runpage(fz_document *doc, int number, fz_document_writer *wri) {
 
 static void runrange(fz_document *doc, const char *range, fz_document_writer *wri) {
     int start, end, count;
-    elog(LOG, "runrange: range=%s", range);
+//    elog(LOG, "runrange: range=%s", range);
     count = fz_count_pages(ctx, doc);
     while ((range = fz_parse_page_range(ctx, range, &start, &end, count))) {
         if (start < end) {
@@ -75,7 +75,7 @@ EXTENSION(pg_mupdf) {
     options = TextDatumGetCString(PG_GETARG_DATUM(3));
     if (PG_ARGISNULL(4)) ereport(ERROR, (errmsg("range is null!")));
     range = TextDatumGetCString(PG_GETARG_DATUM(4));
-    elog(LOG, "pg_mupdf: input_data=%s, input_type=%s, output_type=%s, options=%s, range=%s", VARDATA_ANY(input_data), input_type, output_type, options, range);
+//    elog(LOG, "pg_mupdf: input_data=%s, input_type=%s, output_type=%s, options=%s, range=%s", VARDATA_ANY(input_data), input_type, output_type, options, range);
     fz_try(ctx) {
         output_buffer = fz_new_buffer(ctx, 0);
         fz_set_user_context(ctx, output_buffer);
