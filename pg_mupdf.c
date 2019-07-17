54d044ffa0af589a2a4aaad7caf47dc6b97548b8
RekGRpth <rekgrpth@gmail.com>
2019-07-17 07:45:08 +0500

    up

--- pg_mupdf.c
+++ pg_mupdf.c
@@ -12,7 +12,7 @@
 
 PG_MODULE_MAGIC;
 
-fz_context *context;
+fz_context *ctx;
 
 //static void *mupdf_malloc(void *arg, size_t size) { return palloc(size); }
 //static void *mupdf_realloc(void *arg, void *p, size_t size) { return repalloc(p, size); }
@@ -20,34 +20,51 @@ fz_context *context;
 
 void _PG_init(void); void _PG_init(void) {
 //    fz_alloc_context alloc_context = {NULL, mupdf_malloc, mupdf_realloc, mupdf_free};
-//    if (!(context = fz_new_context(&alloc_context, NULL, FZ_STORE_UNLIMITED))) ereport(ERROR, (errmsg("!fz_new_context")));
-    if (!(context = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED))) ereport(ERROR, (errmsg("!fz_new_context")));
-    fz_try(context) (void)fz_register_document_handlers(context); fz_catch(context) ereport(ERROR, (errmsg("fz_register_document_handlers: %s", fz_caught_message(context))));
-    fz_try(context) (void)fz_set_use_document_css(context, 1); fz_catch(context) ereport(ERROR, (errmsg("fz_set_use_document_css: %s", fz_caught_message(context))));
+//    if (!(ctx = fz_new_context(&alloc_context, NULL, FZ_STORE_UNLIMITED))) ereport(ERROR, (errmsg("!fz_new_context")));
+    if (!(ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED))) ereport(ERROR, (errmsg("!fz_new_context")));
+    fz_try(ctx) (void)fz_register_document_handlers(ctx); fz_catch(ctx) ereport(ERROR, (errmsg("fz_register_document_handlers: %s", fz_caught_message(ctx))));
+    fz_try(ctx) (void)fz_set_use_document_css(ctx, 1); fz_catch(ctx) ereport(ERROR, (errmsg("fz_set_use_document_css: %s", fz_caught_message(ctx))));
 }
 
 void _PG_fini(void); void _PG_fini(void) {
-    (void)fz_drop_context(context);
+    (void)fz_drop_context(ctx);
 }
 
-static void runpage(fz_document *document, int number, fz_document_writer *document_writer) {
+static bool runpage(fz_document *document, int number, fz_document_writer *document_writer) {
     fz_rect mediabox;
     fz_page *page;
     fz_device *dev;
     elog(LOG, "runpage: number=%i", number);
-    fz_try(context) page = fz_load_page(context, document, number - 1); fz_catch(context) ereport(ERROR, (errmsg("fz_load_page: %s", fz_caught_message(context))));
-    fz_try(context) mediabox = fz_bound_page(context, page); fz_catch(context) ereport(ERROR, (errmsg("fz_bound_page: %s", fz_caught_message(context))));
-    fz_try(context) dev = fz_begin_page(context, document_writer, mediabox); fz_catch(context) ereport(ERROR, (errmsg("fz_begin_page: %s", fz_caught_message(context))));
-    fz_try(context) (void)fz_run_page(context, page, dev, fz_identity, NULL); fz_catch(context) ereport(ERROR, (errmsg("fz_run_page: %s", fz_caught_message(context))));
-    fz_try(context) (void)fz_end_page(context, document_writer); fz_catch(context) ereport(ERROR, (errmsg("fz_end_page: %s", fz_caught_message(context))));
-    fz_try(context) (void)fz_drop_page(context, page); fz_catch(context) ereport(ERROR, (errmsg("fz_drop_page: %s", fz_caught_message(context))));
+    page = fz_load_page(ctx, document, number - 1);
+    fz_try(ctx) {
+        mediabox = fz_bound_page(ctx, page);
+        dev = fz_begin_page(ctx, document_writer, mediabox);
+        (void)fz_run_page(ctx, page, dev, fz_identity, NULL);
+        (void)fz_end_page(ctx, document_writer);
+    } fz_always(ctx) {
+        (void)fz_drop_page(ctx, page);
+    } fz_catch(ctx) {
+        fz_rethrow(ctx);
+    }
+    return true;
 }
 
-static void runrange(fz_document *document, const char *range, fz_document_writer *document_writer) {
+static bool runrange(fz_document *document, const char *range, fz_document_writer *document_writer) {
     int start, end, count;
     elog(LOG, "runrange: range=%s", range);
-    fz_try(context) count = fz_count_pages(context, document); fz_catch(context) ereport(ERROR, (errmsg("fz_count_pages: %s", fz_caught_message(context))));
-    while ((range = fz_parse_page_range(context, range, &start, &end, count))) if (start < end) for (int i = start; i <= end; i++) runpage(document, i, document_writer); else for (int i = start; i >= end; i--) runpage(document, i, document_writer);
+    fz_try(ctx) count = fz_count_pages(ctx, document); fz_catch(ctx) ereport(ERROR, (errmsg("fz_count_pages: %s", fz_caught_message(ctx))));
+    while ((range = fz_parse_page_range(ctx, range, &start, &end, count))) {
+        if (start < end) {
+            for (int i = start; i <= end; i++) {
+                runpage(document, i, document_writer);
+            }
+        } else {
+            for (int i = start; i >= end; i--) {
+                runpage(document, i, document_writer);
+            }
+        }
+    }
+    return true;
 }
 
 EXTENSION(pg_mupdf) {
@@ -70,18 +87,18 @@ EXTENSION(pg_mupdf) {
     if (PG_ARGISNULL(4)) ereport(ERROR, (errmsg("range is null!")));
     range = TextDatumGetCString(PG_GETARG_DATUM(4));
     elog(LOG, "pg_mupdf: input_data=%s, input_type=%s, output_type=%s, options=%s, range=%s", VARDATA_ANY(input_data), input_type, output_type, options, range);
-    fz_try(context) input_buffer = fz_new_buffer_from_data(context, (unsigned char *)VARDATA_ANY(input_data), VARSIZE_ANY_EXHDR(input_data)); fz_catch(context) ereport(ERROR, (errmsg("fz_new_buffer_from_data: %s", fz_caught_message(context))));
-    fz_try(context) input_stream = fz_open_buffer(context, input_buffer); fz_catch(context) ereport(ERROR, (errmsg("fz_open_buffer: %s", fz_caught_message(context))));
-    fz_try(context) document = fz_open_document_with_stream(context, input_type, input_stream); fz_catch(context) ereport(ERROR, (errmsg("fz_open_document_with_stream: %s", fz_caught_message(context))));
-    fz_try(context) output_buffer = fz_new_buffer(context, 0); fz_catch(context) ereport(ERROR, (errmsg("fz_new_buffer: %s", fz_caught_message(context))));
-    context->user = output_buffer;
-    fz_try(context) document_writer = fz_new_document_writer(context, "buf:", output_type, options); fz_catch(context) ereport(ERROR, (errmsg("fz_new_document_writer: %s", fz_caught_message(context))));
+    fz_try(ctx) input_buffer = fz_new_buffer_from_data(ctx, (unsigned char *)VARDATA_ANY(input_data), VARSIZE_ANY_EXHDR(input_data)); fz_catch(ctx) ereport(ERROR, (errmsg("fz_new_buffer_from_data: %s", fz_caught_message(ctx))));
+    fz_try(ctx) input_stream = fz_open_buffer(ctx, input_buffer); fz_catch(ctx) ereport(ERROR, (errmsg("fz_open_buffer: %s", fz_caught_message(ctx))));
+    fz_try(ctx) document = fz_open_document_with_stream(ctx, input_type, input_stream); fz_catch(ctx) ereport(ERROR, (errmsg("fz_open_document_with_stream: %s", fz_caught_message(ctx))));
+    fz_try(ctx) output_buffer = fz_new_buffer(ctx, 0); fz_catch(ctx) ereport(ERROR, (errmsg("fz_new_buffer: %s", fz_caught_message(ctx))));
+    ctx->user = output_buffer;
+    fz_try(ctx) document_writer = fz_new_document_writer(ctx, "buf:", output_type, options); fz_catch(ctx) ereport(ERROR, (errmsg("fz_new_document_writer: %s", fz_caught_message(ctx))));
     (void)runrange(document, range, document_writer);
-    (void)fz_drop_buffer(context, input_buffer);
-    (void)fz_drop_document(context, document);
-    (void)fz_close_document_writer(context, document_writer);
-    (void)fz_drop_document_writer(context, document_writer);
-    fz_try(context) output_len = fz_buffer_storage(context, output_buffer, &output_data); fz_catch(context) ereport(ERROR, (errmsg("fz_buffer_storage: %s", fz_caught_message(context))));
+    (void)fz_drop_buffer(ctx, input_buffer);
+    (void)fz_drop_document(ctx, document);
+    (void)fz_close_document_writer(ctx, document_writer);
+    (void)fz_drop_document_writer(ctx, document_writer);
+    fz_try(ctx) output_len = fz_buffer_storage(ctx, output_buffer, &output_data); fz_catch(ctx) ereport(ERROR, (errmsg("fz_buffer_storage: %s", fz_caught_message(ctx))));
     (void)pfree(input_data);
     (void)pfree(input_type);
     (void)pfree(output_type);
